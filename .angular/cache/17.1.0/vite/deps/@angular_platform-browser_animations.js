e5ef8cd101229ba0459aaae37a1173e01dafb799
DevMan Jobson <jobson2084@gmail.com>
2024-01-23 08:53:46 -0300

    NavBar implementada

---
+++ .angular/cache/17.1.0/vite/deps/@angular_platform-browser_animations.js
@@ -0,0 +1,20 @@
+import {
+  BrowserAnimationsModule,
+  InjectableAnimationEngine,
+  NoopAnimationsModule,
+  provideAnimations,
+  provideNoopAnimations
+} from "./chunk-CWJLCZX6.js";
+import "./chunk-YR7I6WKD.js";
+import {
+  ANIMATION_MODULE_TYPE
+} from "./chunk-XG33DMQG.js";
+export {
+  ANIMATION_MODULE_TYPE,
+  BrowserAnimationsModule,
+  NoopAnimationsModule,
+  provideAnimations,
+  provideNoopAnimations,
+  InjectableAnimationEngine as ɵInjectableAnimationEngine
+};
+//# sourceMappingURL=@angular_platform-browser_animations.js.map
